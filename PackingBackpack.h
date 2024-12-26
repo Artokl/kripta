96251fac576fa665773abc7e27012ad6eab9b39b
Artem Okleev <artem.okleev.06@mail.ru>
2024-12-27 00:42:02 +0300

    commit

---
+++ PackingBackpack.h
@@ -0,0 +1,14 @@
+#ifndef PACKINGBACKPACK_H
+#define PACKINGBACKPACK_H
+
+#include <bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+void PackingBackpack();
+pair<int, int> FindCoprimeNumbersFromPrimes(int minProduct);
+int BinaryToDecimal(int binaryNumber);
+
+#endif // PACKINGBACKPACK_H
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("03")
+
+#include <bits/stdc++.h>
+#include "PackingBackpack.h"
+
+using namespace std;
+
+void FastData() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+
+int32_t main()
+{
+//    FastData();
+    PackingBackpack();
+}
